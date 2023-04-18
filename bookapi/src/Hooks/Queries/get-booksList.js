c0958edab9a00b82f831aacfbddece30df09b66c
hyunseoKoo <koohs414@naver.com>
2023-04-18 23:48:30 +0900

    comment: 관심사 분리 주석 추가

--- bookapi/src/Hooks/Queries/get-booksList.js
+++ bookapi/src/Hooks/Queries/get-booksList.js
@@ -2,8 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 import BooksApi from "../../Apis/booksApi"
 import { queryConfig } from "./@config"
 
-
-
+// 전역관리 관심사 분리
 export const useBookList = params => {
     const { data } = useQuery(
         ['GET_LIST'],
