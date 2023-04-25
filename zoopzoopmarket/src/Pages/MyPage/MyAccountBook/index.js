7732c6636712b4b24cba351c49faf678815abc0c
hyunseoKoo <koohs414@naver.com>
2023-04-25 23:51:58 +0900

    feat: 가계부 퍼블리싱

--- zoopzoopmarket/src/Pages/MyPage/MyAccountBook/index.js
+++ zoopzoopmarket/src/Pages/MyPage/MyAccountBook/index.js
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+import AccountBookSelector from "./Components/selector";
+import AccountBookDetailInfo from "./Components/detailInfo";
+
 const AccountBookPage = () => {
-	return <div>ACCOUNT BOOK</div>;
+
+	return(
+	  <S.Wrapper>
+		<AccountBookSelector/>
+		<AccountBookDetailInfo/>
+	  </S.Wrapper>
+	) 
 };
 
 export default AccountBookPage;
+
+const Wrapper = styled.div`
+	margin: 0 auto;
+	/* background-color: beige;	// 확인용 */
+	width: 80vw;
+	height: 100%;
+`;
+
+const S = {
+	Wrapper,
+}
